46efc288e0c6745cda033f1dc8e8d0d680e7901a
Jack Meier <jackson.meier423@gmail.com>
2020-07-30 20:11:48 -0700

    created workout model

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+  },
+  exercises: {
+    type: [],
+  },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
+
+// {
+//     day: new Date(new Date().setDate(new Date().getDate() - 2)),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Military Press",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   }
+
+// {
+//     day: new Date(new Date().setDate(new Date().getDate() - 3)),
+//     exercises: [
+//       {
+//         type: "resistance",
+//         name: "Bench Press",
+//         duration: 20,
+//         weight: 300,
+//         reps: 10,
+//         sets: 4
+//       }
+//     ]
+//   },
