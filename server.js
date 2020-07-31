923cac7692b02e0984b8c2eacbef17f8172c8475
Jack Meier <jackson.meier423@gmail.com>
2020-07-31 09:55:03 -0700

    added middleware

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
