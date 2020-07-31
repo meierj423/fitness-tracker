9f94653781bf55bd703eb5620e18ecae1eac9981
Jack Meier <jackson.meier423@gmail.com>
2020-07-31 09:42:34 -0700

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 app.get("*", (req, res) => {
   res.send("<h1>Hello, World</h1>");
