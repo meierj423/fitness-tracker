44c37bc3e4f1764b53b3c3d785eb1048899fa936
Jack Meier <jackson.meier423@gmail.com>
2020-08-03 20:16:15 -0700

    updated seed.js to work with Heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
